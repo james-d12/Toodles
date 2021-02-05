9709ede3b6b76d72f72cb2cd7cf91237a73d76fd
unknown <imothoth@gmail.com>
2021-02-05 14:25:23 +0000

    Drag and Drop

---
+++ Public/dNd.js
@@ -0,0 +1,22 @@
+const draggables = document.querySelectorAll('.draggable')
+const containers = document.querySelectorAll('.container')
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => { //listens to event of object being dragged
+        //console.log('drag start')
+        draggable.classList.add('dragging')
+    })
+   
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging')
+    })
+})
+
+containers.forEach(container => {
+    container.addEventListener('dragover', e => {
+    e.preventDefault()
+    const draggable = document.querySelector('.dragging')
+    container.appendChild(draggable)
+      //  console.log('dragging over')
+    })
+})
