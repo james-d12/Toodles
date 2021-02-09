647ae92ea7a664a784b88ad2cbd66fa8b0feac80
Fox-Taylor <75842674+Fox-Taylor@users.noreply.github.com>
2021-02-09 12:14:22 +0000

    hello

--- Populate.js
+++ Populate.js
@@ -5,6 +5,7 @@ async function create(){
     
     await classes.User.create({username: "james123", password:"1234", image: "https://www.w3schools.com/howto/img_avatar2.png"})
     await classes.Project.create({name: "Choirs", description: "Keeps track of choirs", image: ""})
+    await classes.Project.create({name: "Choirs 1", description: "Keeps track of choirs", image: ""})
     await classes.Column.create({name: "todo", Pid: 1})
     await classes.Column.create({name: "inprogress", Pid: 1})
     await classes.Column.create({name: "done", Pid: 1})
