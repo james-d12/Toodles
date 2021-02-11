aab70e70e1093638ae33f02f2de448f357341799
unknown <james@gmail.com>
2021-02-11 16:16:41 +0000

    quick fixes

--- Server/Database/SequelizeClasses.js
+++ Server/Database/SequelizeClasses.js
@@ -3,7 +3,7 @@ const {Sequelize, DataTypes, Model} = require('sequelize');
 const sequelize = new Sequelize('database', 'username', 'password', {
     dialect: 'sqlite',
     storage: './Server/Database/Todo.sqlite',
-    logging: true 
+    logging: false
 });
 
 /**
